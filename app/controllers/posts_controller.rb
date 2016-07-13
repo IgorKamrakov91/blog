078b21f0646035ed63f4c6f933a8fbf2b8a11a5e
igor <i.kamrakov@yandex.ru>
2016-07-13 16:58:22 +0300

    Edit and Delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   def new
    @post = Post.new 
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -17,6 +18,26 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
